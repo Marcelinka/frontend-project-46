3968e87d3eaf4ca5c571ccaf63a27c3a3848e16d
Marcelinka <miss.knigomanka@gmail.com>
2024-02-08 00:34:41 +0600

    fix: codeclimate duplication

--- src/helpers/getDiffByKey.js
+++ src/helpers/getDiffByKey.js
@@ -3,17 +3,17 @@ import has from 'lodash/has.js';
 export default function getDiffByKey(file1, file2, key) {
   let str = '';
 
-  if (has(file1, key) && file1[key] !== file2[key]) {
-    str += `  - ${key}: ${file1[key]}\n`;
+  if (file1[key] !== file2[key]) {
+    if (has(file1, key)) {
+      str += `  - ${key}: ${file1[key]}\n`;
+    }
+
+    if (has(file2, key)) {
+      str += `  + ${key}: ${file2[key]}\n`;
+    }
+
+    return str;
   }
 
-  if (has(file2, key) && file2[key] !== file1[key]) {
-    str += `  + ${key}: ${file2[key]}\n`;
-  }
-
-  if (file1[key] === file2[key]) {
-    str += `    ${key}: ${file1[key]}\n`;
-  }
-
-  return str;
+  return `    ${key}: ${file1[key]}\n`;
 }
