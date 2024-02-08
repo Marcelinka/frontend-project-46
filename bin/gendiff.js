c82d0b29ff711fdd4d1b5375000d83a76df842b3
Marcelinka <miss.knigomanka@gmail.com>
2024-02-08 19:20:31 +0600

    feat: pass format from program

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,10 +9,10 @@ program
   .description('Compares two configuration files and shows a difference.')
   .version('0.0.1')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
-    console.log(gendiff(filepath1, filepath2));
+  .action((filepath1, filepath2, options) => {
+    console.log(gendiff(filepath1, filepath2, options.format));
   });
 
 program.parse();
