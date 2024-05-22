dbcc25bb4b75fdf93f454dc1c1539f23a285a961
Marcelinka <miss.knigomanka@gmail.com>
2024-05-22 19:38:25 +0500

    remove unused files

--- src/helpers/stylish.js
+++
@@ -1,5 +0,0 @@
-export const getFirstLine = () => '{\n';
-
-export const getLine = (offset, key, val, sign = ' ') => `${' '.repeat(offset - 2)}${sign} ${key}: ${val}\n`;
-
-export const getLastLine = (offset) => `${' '.repeat(offset - 4)}}`;
